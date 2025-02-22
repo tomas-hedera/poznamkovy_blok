2b3bbc8d1e36e0c59af07a1c1c36e2f443faf732
Tomas Hedera <tomas.hedera@centrum.cz>
2025-02-22 14:07:02 +0100

    chage name login to nodes

--- script.js
+++ script.js
@@ -3,24 +3,24 @@ function showForm(formId) {
         // Skryje registrační formulář
         document.getElementById('registerForm').style.display = 'none';
         // Skryje přihlašovací formulář
-        document.getElementById('loginForm').style.display = 'none';
+        document.getElementById('nodesForm').style.display = 'none';
         // Zobrazí formulář podle předaného ID (formId)
         document.getElementById(formId + 'Form').style.display = 'block';
-
+        
+        // Skryje tlačítka
         document.getElementById('buttons').style.display = 'none';
-
-}
-
-let krizek_registr = document.getElementById('krizek-register');
-krizek_registr.addEventListener('click', (udalost) => {
+    }
+    
+    // Přidání posluchačů událostí pro tlačítka zavření
+    let krizek_registr = document.getElementById('krizek-register');
+    krizek_registr.addEventListener('click', () => {
         document.getElementById('registerForm').style.display = 'none';
-        document.getElementById('loginForm').style.display = 'none';
         document.getElementById('buttons').style.display = 'flex';
-});
+    });
 
-let krizek_login = document.getElementById('krizek-login');
-krizek_login.addEventListener('click', () => {
-        document.getElementById('registerForm').style.display = 'none';
-        document.getElementById('loginForm').style.display = 'none';
+    
+    let krizek_nodes = document.getElementById('krizek-nodes');
+    krizek_nodes.addEventListener('click', () => {
+        document.getElementById('nodesForm').style.display = 'none';
         document.getElementById('buttons').style.display = 'flex';
-});
+    });
