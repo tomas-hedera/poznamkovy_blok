c05600afe807fc41494838a675f67fc55f16d2de
Tomas Hedera <tomas.hedera@centrum.cz>
2025-02-25 21:40:19 +0100

    add note, responsive

--- nodes.php
+++ nodes.php
@@ -13,7 +13,7 @@ include 'db.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/nodes.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Document</title>
+    <title>Poznámky</title>
 </head>
 
 <body>
@@ -25,20 +25,25 @@ include 'db.php';
                 <button class="odhlasit">Odhlásit</button>
             </div>
 
+            <div class="user-panel-mobile">
+                <button name><i class="fa-solid fa-user"></i></button>
+                <button name="hamburger"><i class="fa-solid fa-bars"></i></button>
+            </div>
+
         </header>
 
         <nav>
             <ul>
-                <li><a href="#"><i class="fa-solid fa-clipboard"></i> Přidat poznámku</a></li>
+                <li><a href="#" id="add-note-btn"><i class="fa-solid fa-clipboard"></i> Přidat poznámku</a></li>
                 <li><a href="#"><i class="fa-solid fa-star"></i> Oblíbené</a></li>
                 <li><a href="#"><i class="fa-solid fa-gear"></i> Nastavení</a></li>
             </ul>
         </nav>
 
-        <main>
-            <div class="note">
-                <h2>Název poznámky</h2><br>
-                <p>Obsah poznámky</p><br>
+        <main id="main-content">
+            <div class="note" >
+                <input type="text" name="note-title" id="note-title" value="Název poznámky"><br>
+                <textarea name="note-content" rows="5" cols="40">Obsah poznámky</textarea><br>
                 <button class="edit">Upravit</button>
                 <button class="delete">Odtranit</button>
             </div>
@@ -47,6 +52,7 @@ include 'db.php';
             <p>© 2025 Poznámkový blok</p>
         </footer>
     </div>
+    <script src="script.js"></script>
 </body>
 
 </html>
--- script.js
+++ script.js
@@ -1,26 +1,52 @@
 // Funkce pro zobrazení formuláře
 function showForm(formId) {
-        // Skryje registrační formulář
-        document.getElementById('registerForm').style.display = 'none';
-        // Skryje přihlašovací formulář
-        document.getElementById('nodesForm').style.display = 'none';
-        // Zobrazí formulář podle předaného ID (formId)
-        document.getElementById(formId + 'Form').style.display = 'block';
-        
-        // Skryje tlačítka
-        document.getElementById('buttons').style.display = 'none';
-    }
-    
-    // Přidání posluchačů událostí pro tlačítka zavření
-    let krizek_registr = document.getElementById('krizek-register');
-    krizek_registr.addEventListener('click', () => {
-        document.getElementById('registerForm').style.display = 'none';
-        document.getElementById('buttons').style.display = 'flex';
+    // Skryje registrační formulář
+    document.getElementById('registerForm').style.display = 'none';
+    // Skryje přihlašovací formulář
+    document.getElementById('nodesForm').style.display = 'none';
+    // Zobrazí formulář podle předaného ID (formId)
+    document.getElementById(formId + 'Form').style.display = 'block';
+
+    // Skryje tlačítka
+    document.getElementById('buttons').style.display = 'none';
+}
+
+const addNoteBtn = document.getElementById('add-note-btn');
+const mainContent = document.getElementById('main-content');
+
+// Funkce pro vytvoření nové poznámky
+function addNewNote() {
+    const newNote = document.createElement("div");
+    newNote.classList.add("note");
+
+     // Přidaní obsah do nové poznámky
+     const noteTitle = document.createElement('input');
+     noteTitle.type = "text";
+     noteTitle.name = "note-title";
+     noteTitle.value = "Název poznámky";
+     newNote.appendChild(noteTitle);
+
+    mainContent.appendChild(newNote);                    // Přidat novou poznámku do hlavního obsahu
+
+}
+    addNoteBtn.addEventListener("click", (udalost) => {  //vytvoreni udalosti aby po kliku na tlacitko vytvorilo div (.note)
+        udalost.preventDefault;                          // Zabráníme výchozímu chování odkazu
+        addNewNote();                                    // Přidáme novou poznámku
     });
 
-    
-    let krizek_nodes = document.getElementById('krizek-nodes');
-    krizek_nodes.addEventListener('click', () => {
-        document.getElementById('nodesForm').style.display = 'none';
-        document.getElementById('buttons').style.display = 'flex';
-    });
+
+// Přidání posluchačů událostí pro tlačítka zavření
+let krizek_registr = document.getElementById('krizek-register');
+krizek_registr.addEventListener('click', () => {
+    document.getElementById('registerForm').style.display = 'none';
+    document.getElementById('buttons').style.display = 'flex';
+});
+
+
+let krizek_nodes = document.getElementById('krizek-nodes');
+krizek_nodes.addEventListener('click', () => {
+    document.getElementById('nodesForm').style.display = 'none';
+    document.getElementById('buttons').style.display = 'flex';
+});
+
+
